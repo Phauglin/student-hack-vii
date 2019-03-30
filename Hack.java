595b46c20868f71a90e4c535a6a5a0effd7aff27
Alexander1506 <49095207+Alexander1506@users.noreply.github.com>
2019-03-30 12:45:51 +0000

    Update Hack.java

--- Hack.java
+++ Hack.java
@@ -1,17 +1,20 @@
 import java.awt.*;
 import javax.swing.*;
-public  class Hack extends JFrame{
+public class Hack extends JFrame
+{
 
     // Constructor
     public Hack(){
+        Container contents = getContentPane();
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        Container contents = getContent
         pack();
     }
 
     public static void main(String[] args)
     {
         Hack hack= new Hack();
+        hack.setSize(500, 300);
         hack.setVisible(true);
     }
 }
