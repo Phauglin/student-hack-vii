1872b5020c3a08f99ce06c4b50089ac52203fc70
Yaduraj Bhauruth <44210005+adarshbhauruth@users.noreply.github.com>
2019-03-30 12:30:39 +0000

    Add files via upload

---
+++ Hack.java
@@ -0,0 +1,16 @@
+import java.awt.*
+import javax.swing.*
+public  class Hack{
+
+    // Constructor
+    public Hack(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+    }
+
+    public static void main(String[] args)
+    {
+        Hack hack= new Hack();
+        hack.setVisible(true);
+    }
+}
