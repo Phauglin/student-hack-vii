7a3e586079038e93f06eddad56494af0f220c7e9
Alexander1506 <49095207+Alexander1506@users.noreply.github.com>
2019-03-30 13:47:41 +0000

    Update RenderLayer.java

--- RenderLayer.java
+++ RenderLayer.java
@@ -14,7 +14,7 @@ public class RenderLayer extends Canvas implements Runnable, KeyListener, MouseL
 
     long time;
 
-    ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+    ArrayList<Tom> enemies = new ArrayList<Tom>();
 
     public RenderLayer() //dont touch
     {
@@ -28,60 +28,61 @@ public class RenderLayer extends Canvas implements Runnable, KeyListener, MouseL
     public void run()
     {
       while (true)
-      {
-        try
         {
-          Thread.sleep(17);
-        }
-        catch (InterruptedException e) {}
+            try
+            {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {}
 
-        this.render();
-        this.update();
-        time = System.currentTimeMillis();
-      }
+            this.render();
+            this.update();
+            time = System.currentTimeMillis();
+        }
     }
 
     private void update()
     {
-      for (Enemy e : enemies)
-        e.update();
+    	for (Tom e : enemies)
+    	{
+    		e.update();
+    	}
     }
-
     private void render()
     {
-      // dont touch start
-      BufferStrategy buffer = this.getBufferStrategy();
-      if (buffer == null)
-      {
-        this.createBufferStrategy(3);
-        return;
-      }
-      Graphics g = buffer.getDrawGraphics();
-      // dont touch end
+    	// dont touch start
+        BufferStrategy buffer = this.getBufferStrategy();
+        if (buffer == null)
+        {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = buffer.getDrawGraphics();
+        // dont touch end
 
-      g.setColor(new Color(190,190,190)); // sets background clr
+        g.setColor(new Color(190,190,190)); // sets background clr
 
-      // fillrect takes arguments: xPos, yPos, width, height
+        // fillrect takes arguments: xPos, yPos, width, height
 
-      g.fillRect(0, 0, 1080, 720);
-      for (Enemy e : enemies)
-      {
-      	e.render(g);
-      }
+        g.fillRect(0, 0, 1080, 720);
+        for (Tom e : enemies)
+        {
+        	e.render(g);
+        }
 
-      g.dispose(); //dt
-      buffer.show(); // dt
+        g.dispose(); //dt
+        buffer.show(); // dt
+
     }
 
     public void start() //dt
     {
-      Thread t = new Thread(this);
-      t.start();
+        Thread t = new Thread(this);
+        t.start();
     }
     @Override
-    public void mousePressed(MouseEvent e) // creates new enemy at mouse cursor
+    public void mousePressed(MouseEvent e) // creates new tom at mouse cursor
     {
-    	enemies.add(new Enemy(e.getX(), e.getY()));
+    	enemies.add(new Tom(e.getX(), e.getY()));
     }
 
     @Override
